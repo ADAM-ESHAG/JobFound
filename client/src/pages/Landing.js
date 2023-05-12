4bb4fac2eec74c01ebe53124d23f24e43e106c1e
ADAM-ESHAG <adameshag93@gmail.com>
2023-05-12 10:18:08 +0200

    Adding component folder with indexJs and LogoJs files

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,22 +1,26 @@
-import logo from '../assets/images/logo.svg'
-import main from '../assets/images/main.svg'
+import { Logo } from '../components'
+import main from '../assets/images/main-alternative.svg'
+import Wrapper from '../assets/wrappers/Testing'
+
 const Landing = () => {
   return (
-    <main>
+    <Wrapper>
         <nav>
-            <img src={logo} alt='jobfound' className="logo" />
+           <Logo />
         </nav>
         <div className="container page">
             {/* Info */}
             <div className='info'>
-                <h1>job<pan>tracking</pan> app</h1>
+                <h1>job<span> tracking</span> app</h1>
                 <p>I'm baby viral post-ironic coloring book, chillwave lo-fi bushwick lumbersexual microdosing flexitarian biodiesel chartreuse YOLO. Squid ugh wayfarers gorpcore air plant.</p>
                 <button className="btn btn-hero">Login/Register</button>
             </div>
             <img src={main} alt="job hunt" className="img main-img" />
         </div>
-    </main>
+    </Wrapper>
   )
 }
+
+
 
 export default Landing
