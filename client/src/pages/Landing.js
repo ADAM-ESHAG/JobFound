fd80264d7c70eb009c916eaee70f9c3b3d4439c1
ADAM-ESHAG <adameshag93@gmail.com>
2023-05-15 21:10:01 +0200

    Adding React Router

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,6 +1,7 @@
 import { Logo } from '../components'
 import main from '../assets/images/main-alternative.svg'
 import Wrapper from '../assets/wrappers/Testing'
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
   return (
@@ -13,7 +14,7 @@ const Landing = () => {
             <div className='info'>
                 <h1>job<span> tracking</span> app</h1>
                 <p>I'm baby viral post-ironic coloring book, chillwave lo-fi bushwick lumbersexual microdosing flexitarian biodiesel chartreuse YOLO. Squid ugh wayfarers gorpcore air plant.</p>
-                <button className="btn btn-hero">Login/Register</button>
+                <Link to="/register" className="btn btn-hero">Login/Register</Link>
             </div>
             <img src={main} alt="job hunt" className="img main-img" />
         </div>
