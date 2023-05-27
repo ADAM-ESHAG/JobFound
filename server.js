e24ba4846f66c6c08417d9586f8bca1c69291374
ADAM-ESHAG <adameshag93@gmail.com>
2023-05-27 14:00:58 +0200

    Create db folder, connectJs file and make connection with mongoDB app

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ import express from 'express';
 const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './db/connect.js';
 
 // Middleware
 notFoundMiddleware
 import notFoundMiddleware from './middleware/not-Found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
+import { error } from 'console';
 
 app.get('/', (req, res) => {
-    throw new Error('error');
     res.send('wellcom!');
 })
 
@@ -18,4 +19,13 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is listening on port ${port}...`))
+const start = async() => {
+    try {
+        await connectDB(process.env.MONGO_URL);
+        app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
