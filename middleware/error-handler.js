ca6b9ba652194c4bded43499910151804ccb4e87
ADAM-ESHAG <adameshag93@gmail.com>
2023-06-07 09:12:58 +0200

    Error for unique email

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -11,6 +11,10 @@ const errorHandlerMiddleware = (err, req, res, next) => {
         // defaultError.msg = err.message;
         defaultError.msg = Object.values(err.errors).map((item) => item.message).join(',');
     }
+    if(err.code && err.code === 11000) {
+        defaultError.StatusCode = StatusCodes.BAD_REQUEST;
+        defaultError.msg = `${Object.keys(err.KeyValue)} field has to be unique !`;
+    }
     // res.status(defaultError.StatusCode).json({msg: err});
     res.status(defaultError.StatusCode).json({msg: defaultError.msg});
 }
