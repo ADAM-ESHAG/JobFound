50dd07327e9c3e9fda00ec5232d05482b6f3921a
ADAM-ESHAG <adameshag93@gmail.com>
2023-06-07 09:00:27 +0200

    Missing Field Error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -6,7 +6,13 @@ const errorHandlerMiddleware = (err, req, res, next) => {
         StatusCode: StatusCodes.INTERNAL_SERVER_ERROR,
         msg: 'Something went wrong, try agin later',
     }
-    res.status(defaultError.StatusCode).json({msg: err});
+    if(err.name === 'ValidationError') {
+        defaultError.StatusCode = StatusCodes.BAD_REQUEST;
+        // defaultError.msg = err.message;
+        defaultError.msg = Object.values(err.errors).map((item) => item.message).join(',');
+    }
+    // res.status(defaultError.StatusCode).json({msg: err});
+    res.status(defaultError.StatusCode).json({msg: defaultError.msg});
 }
 
 export default errorHandlerMiddleware
